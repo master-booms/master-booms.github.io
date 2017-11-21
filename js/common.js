9e8dbd5c5dba00c96675bd2bbbac2575039d137f
master-booms <forex1987@gmail.com>
2017-11-21 14:16:16 +0200

    fix landing all pages first commit

--- js/common.js
+++ js/common.js
@@ -1,53 +1,21 @@
 
+var header = $('.header');
 
-
-  //mobile
-  var touch = $('.js__searchBurger');
-  var menu = $('.header__bottom');
- 
-  $(touch).on('click', function(e) {
-    e.preventDefault();
-    menu.slideToggle();
-  });
-  $(window).resize(function(){
-    var w = $(window).width();
-    if(w > 760 && menu.is(':hidden')) {
-      menu.removeAttr('style');
+$(window).scroll(function(e){
+    if(header.offset().top > 120){
+        if(!header.hasClass('shadow')){
+            header.addClass('shadow');
+        }
+    }else{
+        header.removeClass('shadow');
     }
+});
+
+
+
+
+
+
+  $(".service-block__link").mPageScroll2id({
+    offset : 150
   });
-
-
-  //mobile
-  var touch = $('.burger-menu');
-  var menu = $('.header-mnu.js__mnu');
- 
-  $(touch).on('click', function(e) {
-    e.preventDefault();
-    menu.slideToggle();
-  });
-  $(window).resize(function(){
-    var w = $(window).width();
-    if(w > 760 && menu.is(':hidden')) {
-      menu.removeAttr('style');
-    }
-  });
-
-
-
-
-//Fixed Header auto-height
-
-$(".js__sticker").sticky({
-  topSpacing: 1
-})
-
-//jQuery:
-$(".tab__item").not(":first").hide();
-$(".wrapper-tabs .tab").click(function() {
-  $(".wrapper-tabs .tab").removeClass("active").eq($(this).index()).addClass("active");
-  $(".tab__item").hide().eq($(this).index()).fadeIn()
-}).eq(0).addClass("active");
-
-
-
-

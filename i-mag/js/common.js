7f3b16bfc899a4ea12f820d4802daf8320ae7d93
master-booms <forex1987@gmail.com>
2017-12-01 11:38:12 +0200

    fix i-mag main.page

--- i-mag/js/common.js
+++ i-mag/js/common.js
@@ -40,45 +40,28 @@ $(function(){
 
 
 
+$('.product-descr__slider').bxSlider({
+  pagerCustom: '.product-descr__thumb',
+  controls: false
+});
+
+
+$(document).ready(function() {
+  $('.product-descr__slider').magnificPopup({
+
+    delegate: 'a',
+    type:'image',
+    gallery: {
+      enabled: true,
+      tCounter: '' // markup of counter
+    },
+
+
+  });
+});
 
 
 
-// $(document).ready(function() {
-//     $('#registerForm').bootstrapValidator({
-//         message: 'This value is not valid',
-//         feedbackIcons: {
-//             valid: 'glyphicon glyphicon-ok',
-//             invalid: 'glyphicon glyphicon-remove',
-//             validating: 'glyphicon glyphicon-refresh'
-//         },
-//         fields: {
-//             username: {
-//                 message: 'The username is not valid',
-//                 validators: {
-//                     notEmpty: {
-//                         message: 'Поле имя не может быть пустым'
-//                     },
-//                     stringLength: {
-//                         min: 6,
-//                         max: 30,
-//                         message: 'The username must be more than 6 and less than 30 characters long'
-//                     },
-//                     regexp: {
-//                         regexp: /^[a-zA-Z0-9_]+$/,
-//                         message: 'The username can only consist of alphabetical, number and underscore'
-//                     }
-//                 }
-//             },
-//             email: {
-//                 validators: {
-//                     notEmpty: {
-//                         message: 'The email is required and cannot be empty'
-//                     },
-//                     emailAddress: {
-//                         message: 'The input is not a valid email address'
-//                     }
-//                 }
-//             }
-//         }
-//     });
-// });
+
+
+
